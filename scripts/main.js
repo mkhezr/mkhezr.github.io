4698be5e18f4fa76bc0e46d22c246c566b469681
Maryam Khezrzadeh <maryam.khezrzadeh@gmail.com>
2017-07-24 11:20:42 -0700

    Homepage OK. Toxic Beauty OK.

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('#seeall').click(function() {
+    $('.main-container').toggleClass('mobile-nav-open');
+  });
+
+  $('#back').click(function() {
+    $('.main-container').toggleClass('mobile-nav-open');
+  });
+  
+});
